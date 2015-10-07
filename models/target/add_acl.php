a3a2cff148ab5486bfd2c9e42d98c0ff51e3218a
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-07 17:40:43 +0200

    Update documentation

--- models/target/add_acl.php
+++ models/target/add_acl.php
@@ -1,24 +1,32 @@
 <?php
+/**
+ * Documentation for the add_acl() function
+ *
+ * 07.10.2015
+ * Added
+ *
+ */
+
 use phpietadmin\app\models\target;
 
 // require the class
-require_once __DIR__ . '/../../app/models/autoloader.php';
+require_once __DIR__ . '/../registry.php';
 
 // Create object
 // if iqn doesn't exist, it will be created
-$target = new target\Target('iqn.2014-12.com.example.iscsi:test2');
+$target = new target\Target('iqn.2014-12.com.example.iscsi:test1');
 
 // id of acl object in database
-$target->add_acl(1, 'initiators');
+$target->add_acl(2, 'initiators');
 
-print_r($target->get_action_result());
+print_r($target->logging->get_action_result());
 
 // example failure
 /*
 Array
 (
 	[message] => The object 127.0.0.1 is already added!
-[code] => 4
+    [code] => 4
     [code_type] => intern
     [method] => phpietadmin\app\models\target\Target::add_acl
 )

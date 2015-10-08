deec617778ce0c1a621b51703c5b99ee7a3c8dc9
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-08 17:48:15 +0200

    Update documentation

--- models/target/get_acls.php
+++ models/target/get_acls.php
@@ -12,9 +12,35 @@ use phpietadmin\app\models\target;
 // require the class
 require_once __DIR__ . '/../registry.php';
 
-// Create object<
+// Create object
 $target = new target\Target('iqn.2014-12.com.example.iscsi:test1');
 
 $data = $target->get_acls();
 
-print_r($data);
+var_dump($data);
+
+// No acls available
+/*
+bool(false)
+*/
+
+// acls available
+/*
+array(7) {
+  [0]=>
+  string(50) "iqn.2014-12.com.example.iscsi:test1"
+  [1]=>
+  string(3) "ALL"
+  [2]=>
+  string(12) "127.0.0.1"
+  [3]=>
+  string(4) "127.0.0.2"
+  [4]=>
+  string(5) "127.0.0.3"
+  [5]=>
+  string(5) "127.0.0.4"
+  [6]=>
+  string(5) "127.0.0.5"
+}
+
+*/

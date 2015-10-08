eb4fe264935e1bc503831b9901c6df913e514145
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-08 17:58:36 +0200

    Update documentation

--- models/target/return_target_property.php
+++ models/target/return_target_property.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * Documentation for the return_target_property() function
+ *
+ * 08.10.2015
+ * Added
+ *
+ */
+
 use phpietadmin\app\models\target;
 
 // require the class
-require_once __DIR__ . '/../../autoloader.php';
+require_once __DIR__ . '/../registry.php';
 
 // Create object
-// if iqn doesn't exist, it will be created
 $target = new target\Target('iqn.2014-12.com.example.iscsi:test2');
 
 $tid = $target->return_target_property('tid');
